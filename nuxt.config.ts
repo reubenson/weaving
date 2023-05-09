f88b67967ecda56e0cacd9a48989bbbe564c65ca
Reuben Son <reubenson@gmail.com>
2023-05-08 22:22:42 -0400

    try push-dir for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,11 @@ const path = require('path');
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
-  nitro: {
-    output: {
-      publicDir: path.join(__dirname, '/docs')
-    }
-  },
+  // nitro: {
+  //   output: {
+  //     publicDir: path.join(__dirname, '/docs')
+  //   }
+  // },
   modules: [
     '@pinia/nuxt',
     '@element-plus/nuxt'
